21e3904428f87692b8f9299e3a68c66b5a50122d
Justin Ng <91221808+slackng@users.noreply.github.com>
2023-07-04 18:06:25 +0800

    Accept incoming payload as form data

--- src/app/slack/actions/route.ts
+++ src/app/slack/actions/route.ts
@@ -1,14 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-    console.log(request);
     try {
-        const body = await request.json();
-        if (body["type"] === "block_actions") {
-            console.log(body["actions"][0]["action_id"])
-            return new NextResponse(null, { status: 200 });
+        const payload = (await request.formData()).get("payload")?.toString();
+        if (payload) {
+            const body = JSON.parse(payload);
+            if (body["type"] === "block_actions") {
+                console.log(body["actions"][0]["action_id"])
+                return new NextResponse(null, { status: 200 });
+            }
+            return new NextResponse(null, { status: 400 });
         }
-        return new NextResponse(null, { status: 400 });
 
     } catch (e: any) {
         return new NextResponse(JSON.stringify({ error: e.message }), { status: 400 });
