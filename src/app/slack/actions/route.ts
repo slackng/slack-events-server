e9156e7fd0afef869b41b901b041c49708ab6d00
Justin Ng <91221808+slackng@users.noreply.github.com>
2023-07-04 17:46:04 +0800

    Catch errors in body parsing

--- src/app/slack/actions/route.ts
+++ src/app/slack/actions/route.ts
@@ -2,14 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
-    if (body["type"] === "block_actions") {
-        try {
-            console.log(body["action"]["action_id"])
+    try {
+        if (body["type"] === "block_actions") {
+            console.log(body["actions"][0]["action_id"])
             return new NextResponse(null, { status: 200 });
-        } catch (e: any) {
-            return new NextResponse(JSON.stringify({ error: e.message }), { status: 400 });
         }
-    }
+        return new NextResponse(null, { status: 400 });
 
-    return new NextResponse(null, { status: 400 });
+    } catch (e: any) {
+        console.error(body);
+        return new NextResponse(JSON.stringify({ error: e.message }), { status: 400 });
+    }
 }
